0fb8034466385420a735a136f5fd62e7e759559b
Luke Linington <lukelinington@gmail.com>
2026-02-04 21:33:14 +0000

    add category and subcategory seeding

--- backend/database/seed.py
+++ backend/database/seed.py
@@ -3,7 +3,9 @@ from pathlib import Path
 
 sys.path.append(str(Path(__file__).parent.parent))
 
-from backend.models.category_level import CategoryLevel
+from models.sub_category import SubCategory
+from models.category import Category
+from models.category_level import CategoryLevel
 from models.user import User
 
 from models.pin import Pin
@@ -34,8 +36,8 @@ def seed_category_levels():
     db = SessionLocal()
 
     category_levels = [
-        CategoryLevel(cat_level_name="Level 1", cat_level_ttl_mins=60),
-        CategoryLevel(cat_level_name="Level 2", cat_level_ttl_mins=120),
+        CategoryLevel(cat_level_name="Danger", cat_level_ttl_mins=60),
+        CategoryLevel(cat_level_name="Information", cat_level_ttl_mins=120),
         CategoryLevel(cat_level_name="Level 3", cat_level_ttl_mins=180),
     ]
 
@@ -44,6 +46,38 @@ def seed_category_levels():
     db.close()
     print("Category levels seeded")
 
+def seed_categories():
+    print("Seeding categories")
+    db = SessionLocal()
+
+    categories = [
+        Category(cat_name="Theft", cat_level_id=1),
+        Category(cat_name="Anti-Social Behaviour", cat_level_id=2),
+        Category(cat_name="Assault", cat_level_id=3),
+    ]
+
+    db.add_all(categories)
+    db.commit()
+    db.close()
+    print("Categories seeded")
+
+def seed_sub_categories():
+    print("Seeding sub categories")
+    db = SessionLocal()
+
+    sub_categories = [
+        SubCategory(sub_cat_name="Burglary - Theft", cat_id=1),
+        SubCategory(sub_cat_name="Burglary - Mugging", cat_id=1),
+        SubCategory(sub_cat_name="Anti-Social Behaviour - Street", cat_id=2),
+        SubCategory(sub_cat_name="Anti-Social Behaviour - Park", cat_id=2),
+        SubCategory(sub_cat_name="Assault - Physical", cat_id=3),
+        SubCategory(sub_cat_name="Assault - Verbal", cat_id=3),
+    ]
+
+    db.add_all(sub_categories)
+    db.commit()
+    db.close()
+    print("Sub categories seeded")
 
 def seed_pins():
     print("Seeding pins")
@@ -64,6 +98,8 @@ def seed_all():
     print("Start seeding")
     seed_users()
     seed_category_levels()
+    seed_categories()
+    seed_sub_categories()
     seed_pins()
 
 
