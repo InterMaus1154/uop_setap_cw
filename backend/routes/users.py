88e98999d90a6cc7874f3de5af9a75c743b5fec5
InterMaus1154 <amarco1154@gmail.com>
2026-02-17 12:00:15 +0000

    add users/me route for getting user profile

--- backend/routes/users.py
+++ backend/routes/users.py
@@ -3,10 +3,12 @@ from fastapi.params import Depends
 from sqlalchemy.orm import Session, Query as Q
 
 from database.db import get_db
+from middleware.auth import require_auth
 from models.user import User
 from schemas.User import UserResponse, UserCreate
 
 router = APIRouter(prefix="/users", tags=["users"])
+
 
 # Below are some example routes for getting user and creating new user
 
@@ -15,6 +17,13 @@ def get_users(db: Session = Depends(get_db)):
     """List all users. Used for prototype only."""
     users = db.query(User).all()
     return users
+
+
+@router.get('/me', response_model=UserResponse)
+def get_me(user: User = Depends(require_auth)):
+    """Return profile data for the logged-in user"""
+    return user
+
 
 @router.get('/{user_id}', response_model=UserResponse)
 def get_user_by_id(user_id: int, db: Session = Depends(get_db)):
@@ -26,8 +35,9 @@ def get_user_by_id(user_id: int, db: Session = Depends(get_db)):
 
     return user
 
+
 @router.get('/search/{email}', response_model=UserResponse)
-def get_user_by_email(email: str, db : Session = Depends(get_db)):
+def get_user_by_email(email: str, db: Session = Depends(get_db)):
     user = db.query(User).filter(User.user_email == email).first()
 
     if not user:
@@ -35,9 +45,9 @@ def get_user_by_email(email: str, db : Session = Depends(get_db)):
 
     return user
 
+
 @router.post('/', response_model=UserResponse, status_code=201)
 def create_user(user_data: UserCreate, db: Session = Depends(get_db)):
-
     # check for email
     existing_user = db.query(User).filter(User.user_email == user_data.user_email).first()
 
@@ -45,10 +55,10 @@ def create_user(user_data: UserCreate, db: Session = Depends(get_db)):
         raise HTTPException(status_code=422, detail="User with email already exists")
 
     new_user = User(
-        user_email = user_data.user_email,
-        user_fname = user_data.user_fname,
-        user_lname = user_data.user_lname,
-        user_displayname = user_data.user_displayname
+        user_email=user_data.user_email,
+        user_fname=user_data.user_fname,
+        user_lname=user_data.user_lname,
+        user_displayname=user_data.user_displayname
     )
 
     db.add(new_user)
