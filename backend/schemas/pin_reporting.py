27ebe8ba07d01ea56c0328eae51790450f02f369
Toby <master.toby.webb@gmail.com>
2026-02-23 16:37:26 +0000

    Add pin reporting request schema

--- backend/schemas/pin_reporting.py
+++ backend/schemas/pin_reporting.py
@@ -1,1 +1,7 @@
+from pydantic import BaseModel
+from models.pin_report import PinReportType
 
+
+class PinReportRequest(BaseModel):
+    """Data required when reporting a pin"""
+    report_type: PinReportType
