f1cf18d400d7b6c4666f358df2c203d69f51563b
Julian Brooks Newman <julian.13bn@gmail.com>
2026-03-01 21:39:11 +0000

    fixed code to match master

--- backend/models/user_location.py
+++ backend/models/user_location.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, BigInteger, ForeignKey, Float, Boolean, DateTime, func
+from sqlalchemy import Column, BigInteger, ForeignKey, DOUBLE_PRECISION, Boolean, DateTime, func
 from sqlalchemy.orm import relationship
 
 from database.db import Base
@@ -9,8 +9,8 @@ class UserLocation(Base):
 
     user_loc_id = Column(BigInteger, primary_key=True, autoincrement=True, index=True)
     user_id = Column(BigInteger, ForeignKey("users.user_id"), nullable=False, unique=True)
-    latitude = Column(Float, nullable=False)
-    longitude = Column(Float, nullable=False)
+    latitude = Column(DOUBLE_PRECISION, nullable=False)
+    longitude = Column(DOUBLE_PRECISION, nullable=False)
     is_enabled = Column(Boolean, nullable=False, default=True, server_default="true")
     updated_at = Column(DateTime, nullable=False, onupdate=func.now(), default=func.now(), server_default=func.now())
     created_at = Column(DateTime, nullable=False, default=func.now(), server_default=func.now())
