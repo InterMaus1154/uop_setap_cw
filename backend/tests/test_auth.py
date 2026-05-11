4045a7d70cbcc4099099964ba1f4528cfcd16495
InterMaus1154 <amarco1154@gmail.com>
2026-05-11 19:58:09 +0100

    test auth update

--- backend/tests/test_auth.py
+++ backend/tests/test_auth.py
@@ -1,8 +1,10 @@
 import pytest
-from base import client
+
 
 class TestAuth:
-    def test_login_with_valid_credentials(self):
+    def test_login_with_valid_credentials(self, client):
+        """AUTH"""
+
         response = client.post("/auth/login", json={"email": "test@test.app"})
 
         data = response.json()
@@ -11,20 +13,18 @@ class TestAuth:
         assert "token" in data
         assert len(data["token"]) > 0
 
-    def test_login_with_invalid_credentials(self):
+    def test_login_with_invalid_credentials(self, client):
         response = client.post("/auth/login", json={"email": "invalidmail@test.com"})
 
         assert response.status_code == 401
 
-    def test_logout_with_valid_token(self, auth_headers):
+    def test_logout_with_valid_token(self, auth_headers, client):
         response = client.post("/auth/logout", headers=auth_headers)
 
         assert response.status_code == 200
 
-        # re-login with test user to have the token for further tests
-        client.post("/auth/login", json={"email": "test@test.app"})
 
-    def test_logout_with_invalid_token(self):
+    def test_logout_with_invalid_token(self, client):
         response = client.post("/auth/logout", headers={"Authorization": "Bearer sdghsdfhsdfh"})
 
         assert response.status_code == 401
